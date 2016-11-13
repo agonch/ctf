ea3318fc8d71541c1b541e053a025b46766e0d4d
Anton Goncharenko <agonch@.cs.washington.edu>
2016-11-13 15:31:48 -0800

    controller and ibndex

---
+++ src/client/js/controller.js
@@ -0,0 +1,17 @@
+/**
+ * Created by agonch on 11/13/16.
+ */
+
+/*
+ * This is the controller on the client side.
+ * This will listen to server broadcasts and messages, and send updates to the server.
+ * This will use some UI API to call the associated view methods (to update a view).
+ *
+ * No UI logic, no game logic ... just communication with server.
+ */
+
+var socket = io.connect('localhost');
+
+socket.on('ack', function(data) {
+    console.log(data);
+});

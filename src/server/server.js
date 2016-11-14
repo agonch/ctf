a08d5c53646861a695c001bc394d5d06aa186340
Anton Goncharenko <agonch@.cs.washington.edu>
2016-11-13 16:10:25 -0800

    serving files

--- src/server/server.js
+++ src/server/server.js
@@ -1,20 +1,30 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require('path');
 
 
 var names = [];
 
 http.listen(3000, function() {
     console.log('listening on *:3000');
-})
+});
 
 app.get('/', function (req, res) {
     console.log('serving');
-    res.sendfile(__dirname + '/../client/index.html');
-    res.sendfile(__dirname + '/../client/js/controller.js');
-    res.sendfile(__dirname + '/../client/js/canvas.js');
+    res.setHeader("Content-Type", "text/html");
+    res.sendFile(path.join(__dirname, '../client/index.html'));
 });
+
+// app.get('/client/js/canvas.js', function (req, res)  {
+//     res.setHeader("Content-Type", "text/javascript");
+//     res.sendFile(path.join(__dirname, '../client/js/canvas.js'));
+// });
+//
+// app.get('/client/js/controller.js', function (req, res)  {
+//     res.setHeader("Content-Type", "text/javascript");
+//     res.sendFile(path.join(__dirname, '../client/js/controller.js'));
+// });
 
 io.on('connection', function (socket) {
     console.log("New connection");
